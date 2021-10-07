4298fb72bcf20c0931a8a3613144f0ba1e3943e6
Marcel Keller <mks.keller@gmail.com>
2021-10-07 14:16:46 +1100

    Launcher.

--- examples/mpc_autograd_cnn/launcher.py
+++ examples/mpc_autograd_cnn/launcher.py
@@ -87,7 +87,7 @@ def _run_experiment(args):
         level=level,
         format="%(asctime)s - %(process)d - %(name)s - %(levelname)s - %(message)s",
     )
-    from mpc_autograd_cnn import run_mpc_autograd_cnn
+    from net_a import run_mpc_autograd_cnn
 
     run_mpc_autograd_cnn(
         num_epochs=args.epochs,
