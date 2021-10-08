a52c01c0971970a7588ed680351e382f1e18f907
Marcel Keller <mks.keller@gmail.com>
2021-10-08 17:22:02 +1100

    Match parameters.

--- examples/mpc_autograd_cnn/nets.py
+++ examples/mpc_autograd_cnn/nets.py
@@ -200,17 +200,18 @@ class NetA(nn.Module):
 class LeNet(nn.Module):
     def __init__(self):
         super(LeNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(800, 500)
+        self.fc2 = nn.Linear(500, 10)
 
     def forward(self, x):
         x = self.conv1(x)
+        x = F.max_pool2d(x, 2)
         x = F.relu(x)
         x = self.conv2(x)
-        x = F.relu(x)
         x = F.max_pool2d(x, 2)
+        x = F.relu(x)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
         x = F.relu(x)
